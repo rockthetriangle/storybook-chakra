1b0bd280889d885484b0713a5c973a21c65f9d75
Bilal Atique <bilalbinatique@gmail.com>
2025-02-27 22:19:01 +0500

    added edit icon with avatar

--- src/components/molecules/UserCard.tsx
+++ src/components/molecules/UserCard.tsx
@@ -1,9 +1,5 @@
-import {
-    Avatar,
-    HStack,
-    Stack,
-    Text
-} from "@chakra-ui/react";
+import { Avatar, Circle, Float, HStack, Stack, Text } from "@chakra-ui/react";
+import { MdOutlineEdit } from "react-icons/md";
 
 interface User {
   id: string;
@@ -15,9 +11,14 @@ interface User {
 export const UserCard = ({ user }: { user: User }) => {
   return (
     <HStack gap="4">
-      <Avatar.Root size="md" variant="solid">
+      <Avatar.Root size="2xl" variant="solid" shape="square">
         <Avatar.Fallback name={user.name} />
         <Avatar.Image src={user.avatar} />
+        <Float placement="bottom-end" offsetX="2" offsetY="2">
+          <Circle bg="gray.400" size="20px">
+            <MdOutlineEdit style={{ padding: "2px" }} />
+          </Circle>
+        </Float>
       </Avatar.Root>
       <Stack gap="1">
         <Text fontWeight="medium">{user.name}</Text>
